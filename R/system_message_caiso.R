aa91d8ea0e80ba6e11bab725124f2209a12c82e1
Randy <randy.joseph.avalos@gmail.com>
2024-01-04 16:30:57 -0800

    Fix raw unzip method Add result format for csv

--- R/system_message_caiso.R
+++ R/system_message_caiso.R
@@ -1,7 +1,8 @@
 system_message_caiso <- function(
     from_date = Sys.Date()-14,
     to_date = Sys.Date()+1,
-    message_severity = c("ALL"),
+    message_severity = c("ALL", "Emergency", "Urgent", "Normal"),
+    resultformat = c("6", "5"),
     base_url = "http://oasis.caiso.com/oasisapi",
     tz_hrs_from_gmt = "0700",
     api_version = "1"
@@ -23,7 +24,7 @@ system_message_caiso <- function(
   req <- request(base_url) |>
     req_url_path_append("SingleZip") |>
     req_url_query(
-      queryname = "ATL_OS",
+      queryname = "ATL_OSM",
       msg_severity = message_severity,
       startdatetime = msg_start_datetime,
       enddatetime = msg_end_datetime,
@@ -35,7 +36,7 @@ system_message_caiso <- function(
 
   # Extract (unzip csv) and load results
   temp_file <- tempfile() # create temp file
-  writeBin(content(resp), temp_file) # unzip to temp file
+  writeBin(resp_body_raw(resp), temp_file) # unzip to temp file
   df <- read_csv(temp_file) # read temporary file
   file.remove(temp_file) # remove temp file
 
